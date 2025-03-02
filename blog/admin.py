a5626ff65d1aaefaef20bc20a79b7fb89ef22400
masm14 <melaniesalloum1@gmail.com>
2025-03-02 13:56:15 -0400

    model Publication added

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Publications
 
 # Register your models here.
+class PublicationsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date', 'updated')
+    list__filter = ('author', 'date', 'updated')
+    search_fields = ('title', 'body', 'author__username')
+    ordering = ('-date',)
+    
+
+admin.site.register(Publications, PublicationsAdmin)
